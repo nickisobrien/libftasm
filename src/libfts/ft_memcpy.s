deda09c0c23df26ebde37bae8b4c9fd765479498
Nick <hoop.bbb@gmail.com>
2019-01-15 19:12:36 -0800

    added memcpy

--- src/libfts/ft_memcpy.s
+++ src/libfts/ft_memcpy.s
@@ -1,3 +1,19 @@
 global _ft_memcpy
 
 _ft_memcpy:
+init:
+	mov rax, rdx
+	jmp loop
+
+loop:
+	cmp rax, 0x0
+	je end
+	mov cl, byte [rsi]
+	mov byte [rdi], cl
+	inc rdi
+	inc rsi
+	dec rax
+	jmp loop
+
+end:
+	ret
